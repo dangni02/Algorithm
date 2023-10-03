7b30521ff729d0ce4cd83dfac6aa97463fc8413a
dangni02 <102530261+dangni02@users.noreply.github.com>
2023-10-03 19:20:51 +0900

    [Silver V] Title: 색종이, Time: 212 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int [][]  paper = new int[100][100];
+        for(int i=0; i<N; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    paper[j][k] = 1;
+                }
+            }
+        }
+
+        // 전체 면적 계산
+        int area = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if(paper[i][j] == 1){
+                    area++;
+                }
+            }
+        }
+
+        System.out.println(area);
+    }
+}
